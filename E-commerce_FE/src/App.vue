8c6ac89de588f6606c017be3fd5f16b6c0cd9cd5
Dang_Duong_Huy <147192155+fillbertRiko@users.noreply.github.com>
2025-05-30 23:58:00 +0700

    feat: initialize E-commerce frontend with Vue 3, Vite, and Tailwind CSS
    - Added package.json with dependencies for Vue, Pinia, and Vue Router
    - Configured PostCSS with Tailwind CSS and Autoprefixer
    - Created main application structure with App.vue, main.js, and router setup
    - Implemented basic routing for Home and Register views
    - Established a Pinia store for counter state management
    - Designed basic styles using Tailwind CSS in main.css
    - Set up Vite configuration with development server and proxy settings

---
+++ E-commerce_FE/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {RouterLink, RouterView} from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to ="/" class="nav-link">Home</RouterLink>
+      <div>
+        <RouterLink to="/register" class="nav-link">Register</RouterLink>
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
